4db068495f883085d2d40e70924e6f5c049bcb08
Cristhian Rengifo <cristhian860@hotmail.com>
2025-09-14 14:27:16 -0500

    chore: creacion Layout Components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 
 
@@ -6,15 +7,21 @@ export const countryRoutes: Routes = [
 
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      },
+    ]
   },
   // {
   //   path: 'country',
   //   // component: HomePageComponent
-  // },
-  // {
-  //   path: '**',
-  //   redirectTo: ''
   // },
 
 ];
