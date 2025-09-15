f1b45bcf1726ed75554d66a6bd1262607802ccf7
Cristhian Rengifo <cristhian860@hotmail.com>
2025-09-14 22:24:16 -0500

    feat: se creo una interfaz y se muestran los datos en la tabla by-capital

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
+import { RESTCountry } from '../interfaces/rest-countries.interface';
+
 const API_URL = 'https://restcountries.com/v3.1';
 
 @Injectable({
@@ -13,6 +15,6 @@ export class CountryService {
   searchByCapital( query: string ) {
     query = query.toLowerCase();
 
-    return this.http.get(`${API_URL}/capital/${query}`);
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`);
   }
 }
