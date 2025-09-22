656792176db4c6d07ca66f9a0e0ae9e3a586da12
Cristhian Rengifo <cristhian860@hotmail.com>
2025-09-21 23:22:21 -0500

    feat: implementacion busqueda por pais

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { firstValueFrom } from 'rxjs';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country-page',
@@ -8,5 +10,18 @@ import { CountryListComponent } from "../../components/country-list/country-list
   templateUrl: './by-country-page.component.html',
 })
 export class ByCountryPageComponent {
-  
+
+  CountryService = inject(CountryService);
+  query = signal('');
+
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
+
+      return await firstValueFrom(
+        this.CountryService.searchByCountry(request.query)
+      );
+    },
+  });
 }
