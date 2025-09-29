db42e3ee73fb683d6f4268feb7956f541396ed2d
Cristhian Rengifo <cristhian860@hotmail.com>
2025-09-29 16:17:54 -0500

    feat: detalles pais

--- src/app/country/mappers/country.mapper.ts
+++ src/app/country/mappers/country.mapper.ts
@@ -12,7 +12,10 @@ export class CountryMapper {
       flag: restCountry.flag,
       flagSvg: restCountry.flags.svg,
       name: restCountry.translations['spa'].common ?? 'No Spanish Name',
-      population: restCountry.population
+      population: restCountry.population,
+      
+      region: restCountry.region,
+      subregion: restCountry.subregion,
     }
   }
 
